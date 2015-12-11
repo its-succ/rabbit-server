7c79b75a730639d33178a259aafa013c133209af
SAKAI Yoshihito <y-sakai@esm.co.jp>
2015-12-11 11:34:17 +0900

    mainをapp.jsに変更

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/rabbit');
+var db = mongoose.connection;
+db.once('open', function(callback) {
+  console.log('db connected');
+});
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -7,9 +15,12 @@ app.use(bodyParser.json());
 var children = require('./children');
 app.use('/api/children', children);
 
+var cards = require('./cards');
+app.use('/api/cards', cards);
+
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
-    
+
     console.log('listening at http://%s:%s', host, port);
 });
