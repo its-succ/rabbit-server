5abe1843035fdb3b21e86d18c4fd6b48c72f588d
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2016-01-29 12:21:15 +0900

    Implement all-search feature, Include cards into result of single-search

--- children.js
+++ children.js
@@ -3,15 +3,32 @@ var router = express.Router();
 
 var Child = require('./child').Child;
 var Card = require('./card').Card;
-var Check = require('./check').Card;
-var CheckHistory = require('./check_history').CheckHistory;
+
+router.get('/', function (req, res) {
+  Child.find({}, '_id name birthday sex', function (err, children) {
+    if (children) {
+      res.json(children);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
 
 router.get('/:id', function (req, res) {
   var id = req.params.id;
   console.log('ID: ' + id);
   Child.findById(id, function (err, child) {
     if (child) {
-      res.json(child);
+      Card.find({ children: id }, function(err, cards) {
+        child.cards = cards;
+        res.json({
+          _id: child._id,
+          name: child.name,
+          birthday: child.birthday,
+          sex: child.sex,
+          cards: cards
+        });
+      });
     } else {
       res.sendStatus(404);
     }
@@ -26,30 +43,30 @@ router.post('/', function (req, res) {
       res.sendStatus(500);
     } else {
       console.log(child);
-      res.send('Child Registered. ID: ' + child._id);
+      res.sendStatus(200);
     }
   });
 });
 
 router.put('/:id', function (req, res) {
-  Child.update({_id: req.params.id}, req.body, {}, function (err, raw) {
+  Child.findByIdAndUpdate(req.params.id, req.body, function (err, child) {
     if (err) {
       console.error(err);
       res.sendStatus(500);
     } else {
-      console.log(raw);
-      res.send('Child updated.');
+      console.log(child);
+      res.sendStatus(200);
     }
   });
 });
 
 router.delete('/:id', function (req, res) {
-  Child.remove({_id: req.params.id}, function (err) {
+  Child.findByIdAndRemove(req.params.id, function (err) {
     if (err) {
       console.error(err);
       res.sendStatus(500);
     } else {
-      res.send('Child deleted.');
+      res.sendStatus(200);
     }
   });
 });
